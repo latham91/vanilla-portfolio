4aa7a5528d8b06be52285a01c766cb2833e310e6
Aaron Latham <aaron@Aarons-Mac-mini.local>
2023-12-27 14:24:13 +0000

    Added functional mobile navbar and changed modal when in mobile view

--- script.js
+++ script.js
@@ -3,18 +3,45 @@ const modal = document.querySelector(".modal-bg");
 const closeBtn = document.querySelector(".btn-close");
 const modalSend = document.querySelector(".btn-send");
 
+const burgerMenu = document.querySelector(".burger-menu");
+const navLinks = document.querySelector(".nav-links");
+const navLinksList = document.querySelectorAll(".nav-links a");
+
 contactBtn.addEventListener("click", () => {
-  modal.classList.remove("modal-hidden");
-  modal.classList.add("modal-visible");
+    modal.classList.remove("modal-hidden");
+    modal.classList.add("modal-visible");
+    navLinks.classList.remove("active");
 });
 
 closeBtn.addEventListener("click", () => {
-  modal.classList.add("modal-hidden");
-  modal.classList.remove("modal-visible");
+    modal.classList.add("modal-hidden");
+    modal.classList.remove("modal-visible");
 });
 
 modalSend.addEventListener("click", () => {
-  alert("Message sent!");
-  modal.classList.add("modal-hidden");
-  modal.classList.remove("modal-visible");
+    alert("Message sent!");
+    modal.classList.add("modal-hidden");
+    modal.classList.remove("modal-visible");
+});
+
+// Toggle navigation menu
+burgerMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Close navigation menu when a link is clicked
+navLinksList.forEach((link) => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
+
+// Close navigation menu when clicking outside of it
+document.addEventListener("click", (event) => {
+    const isClickInsideNav = navLinks.contains(event.target);
+    const isClickOnBurger = burgerMenu.contains(event.target);
+
+    if (!isClickInsideNav && !isClickOnBurger) {
+        navLinks.classList.remove("active");
+    }
 });
